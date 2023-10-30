3cc8ddc0ef792197f6b9ef978d22f9e317c42d68
Dani Rojas <daniela.rojas@selu.edu>
2023-10-30 14:48:13 +0000

    Funciones modificadas

--- Rojas.pkg/R/Filter.R
+++ Rojas.pkg/R/Filter.R
@@ -1,1 +1,25 @@
-# this function is for filter the characteristic data
+#' This function is for filtering data 
+#' 
+#' Use for filter the hormones concentration that is on the columns in the data set.
+#' Returns hormone concentration of the filter data request. 
+#' 
+#'@param data in the date is expecting that each hormone is in each column
+#' @param column_3 concentration of an hormone
+#' @param column_4 concentration of an hormone
+#' @return filter columns with the concentration of VTG and E2 (numeric)
+#' 
+#'@export
+
+
+filter_H <- function(data, column_3, column_4){
+  if(is.numeric({{column_3}}) == TRUE) {
+  filter <- data %>% 
+    select({{column_3}}, {{column_4}})
+  return(filter)
+  } else {
+    print("NO")
+  }
+}
+
+filter_H(Data_RojasC, VTG, E2)
+
--- Rojas.pkg/R/Select.R
+++ Rojas.pkg/R/Select.R
@@ -1,7 +1,23 @@
+#' This function is for select specific data 
+#' 
+#' Use for select the hormone concentration columns that arein data set.
+#' Returns concentration values of the selected hormones.
+#'  
+#' @param data in the date is expecting that each hormone is in each column
+#' @param column_3 concentration of an hormone
+#' @param column_4 concentration of an hormone
+#' @param column_5 concentration of an hormone
+#' @param column_6 concentration of an hormone
+#' @return columns the concentration of the hormones (numeric)
+#' 
+#'@export
+
+
+
 function_DR <- function(data, column_3, column_4, column_5, column_6){
   column_select <- data %>% 
     select({{column_3}}, {{column_4}}, {{column_5}}, {{column_6}})
   return(column_select)
 }
 
-# this function is made to select specific data to see it. 
+function_DR(Data_RojasC, VTG, E2, Testosterone, BHB)
